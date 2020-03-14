7588945154f813383635a674ee2d49c48d88be03
John Paul Ada <johnpaulada@gmail.com>
2020-03-14 20:34:53 +0800

    Initialize assignment

---
+++ src/server/src/resolvers.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+// TODO: Connect to mongo here
+
+// TODO: Replace `{}` with actual Movie model
+const Movie = {}
+
+const getMovieList = async () => {
+  return await Movie.find({})
+}
+
+const resolvers = {
+    Query: {
+      movies: async () =>  {
+        const movieList = await getMovieList()
+        return movieList
+      }
+    },
+  };
+  
+export default resolvers;
