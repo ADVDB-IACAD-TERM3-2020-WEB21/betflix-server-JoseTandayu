2270441d8a184310703a692c5521d6db5ab63526
JoseTandayu <61930017+JoseTandayu@users.noreply.github.com>
2020-03-30 11:48:55 +0800

    Add files via upload

--- src/server/src/resolvers.js
+++ src/server/src/resolvers.js
@@ -1,12 +1,34 @@
 // TODO: Import Mongoose here
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://127.0.0.1:27017/my_database';
+
+
 // TODO: Connect to mongo here
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+
 
 // TODO: Replace `{}` with actual Movie model
-const Movie = {}
+var Schema = mongoose.Schema;
+
+const Movie = new Schema({
+  title: String
+});
+
+var Movies = mongoose.model('movies', Movie);
+// module.exports = mongoose.model('Movie', Movie);
+
+
+
 
 const getMovieList = async () => {
-  return await Movie.find({})
-}
+  return await Movies.find({});
+};
 
 const resolvers = {
     Query: {
